86b4df17f052d90cc63906173481e47d985f4d73
BryonVasquez <143046378+BryonVasquez@users.noreply.github.com>
2024-07-27 20:02:32 +0000

    Question D

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,20 @@
-#add import
+from question_d import Stock
+ 
+def main():
+    
+    stocks = [
+        Stock("AAPL", "Apple Inc"),
+        Stock("CAT", "Caterpillar"),
+        Stock("EK", "Eastman Kodak"),
+        Stock("GOOG", "Google"),
+        Stock("MSFT", "Microsoft")
+    ]
+
+    
+    print(f"{'Symbol':<10} {'Company Name'}")
+    print("-" * 30)
+    
+    for stock in stocks:
+        print(f"{stock.get_symbol():<10} {stock.get_company_name()}")
+
+main()
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,23 @@
-#write functions here, don't add input('') statements here!
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    
+    def get_symbol(self):
+        return self.__symbol
+
+
+    def get_company_name(self):
+        return self.__company_name
+
+def display_stock_report(stocks):
+
+    print(f"{'Company':<20} {'Symbol':<10}")
+    print("-" * 30)
+    
+
+    for stock in stocks:
+        print(f"{stock.get_company_name():<20} {stock.get_symbol():<10}")

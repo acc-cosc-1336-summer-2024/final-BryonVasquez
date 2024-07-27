66a6adc256fcff95bea2153a5ae64df877ebc1e7
BryonVasquez <143046378+BryonVasquez@users.noreply.github.com>
2024-07-27 19:50:57 +0000

    Question C

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,21 @@
-#add import
+
+from question_c import stock_purchase_history
+
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1 - Display stock purchase history")
+        print("2 - Exit")
+        
+        option = input("Please select an option: ").strip()
+
+        if option == '1':
+            stock_purchase_history()
+        elif option == '2':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option. Please select a valid option from the menu.")
+
+main()
